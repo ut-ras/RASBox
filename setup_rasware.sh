ea4ef08da4d29256663944ee9a60d765be7979e7
Matthew Yu <31521956+dimembermatt@users.noreply.github.com>
2020-07-15 04:37:17 -0500

    Update quotes and clarified switch on tm4c

--- setup_rasware.sh
+++ setup_rasware.sh
@@ -22,8 +22,8 @@ make
 cd ../RASDemo
 make
 
-echo -e "\n\nReady to go! To test to see if your setup is correct, cd into the RASDemo folder (\`cd ~/RASBox/Rasware/RASDemo\`), plug in your TM4C through the USB, turn it on with debug mode, and call 'make flash'!"
+echo -e "\n\nReady to go! To test to see if your setup is correct, cd into the RASDemo folder ('cd ~/RASBox/Rasware/RASDemo'), plug in your TM4C through the USB, flip the switch to debug mode, and call 'make flash'!"
 echo "If your make flash results in an error, try reconnecting and resetting the device, and try again."
 echo "The TM4C may not appear to do anything at first. Try pressing the reset button and it should start blinking green."
-echo "For further help, consult a RAS leader (\`'@Robotic Fish\`' on slack)."
-echo "Now jump back to the RASbox User Guide using razzle!"
+echo "For further help, consult a RAS leader ('@Robotic Fish' on slack)."
+echo "Now jump back to the RASbox User Guide using 'razzle'!"
